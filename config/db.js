b9da00531035ed15b3be7e7d01148512daf936a0
adityanashtech <aditya.gangwar@nashtechglobal.com>
2025-06-24 14:31:06 +0530

    sdcsdc

--- config/db.js
+++ config/db.js
@@ -1,8 +1,8 @@
 const mysql = require("mysql2");
 const connection = mysql.createConnection({
   host: "localhost",
-  user: "root", // Replace with your MySQL username
-  password: "india0192", // Replace with your MySQL password
+  user: "lucifer", // Replace with your MySQL username
+  password: "Welcome@noida2024", // Replace with your MySQL password
   database: "stake",
 });
 
