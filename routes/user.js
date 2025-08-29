5f550b804e580745c284034244685e0aeda9c9e5
adityanashtech <aditya.gangwar@nashtechglobal.com>
2025-08-29 17:07:36 +0530

    url chnaged

--- routes/user.js
+++ routes/user.js
@@ -2283,7 +2283,7 @@ router.get("/referrals/today-summary/:userId", async (req, res) => {
           u.username,
           u.email,
           r.level,
-          (SELECT d.amount FROM deposits d WHERE d.userId = u.id AND DATE(d.created_at) = ? ORDER BY d.created_at ASC LIMIT 1) AS first_deposit,
+          (SELECT d.amount FROM deposits d WHERE d.userId = u.id AND DATE(d.created_at) = ? ORDER BY d.date ASC LIMIT 1) AS first_deposit,
           (SELECT SUM(d.amount) FROM deposits d WHERE d.userId = u.id AND DATE(d.created_at) = ?) AS total_deposit,
           (SELECT SUM(b.amount) FROM bets b WHERE b.user_id = u.id) AS total_bets,            
           (SELECT IFNULL(SUM(c.amount), 0) FROM referralcommissionhistory c WHERE c.user_id = ? AND c.referred_user_id = u.id AND c.credited = 0) AS pending_commission
